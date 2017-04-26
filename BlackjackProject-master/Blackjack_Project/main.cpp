254285d3f72a1d53f9648710c1185274af839779
kabeerkler <klerks@mail.uc.edu>
2017-04-26 16:42:27 -0400

    Add files via upload

---
+++ BlackjackProject-master/Blackjack_Project/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+
+
+#include <player.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    vector<int> deck;
+    int players = 12;
+    while(players > 0 && players < 11)
+    {
+        cout << "How many Players? (up to 8) ";
+        cin >> players;
+    }
+
+    for(int x=0;x<13;x++)
+    {
+
+    }
+
+
+
+
+
+
+
+    Player* p1 = new Player(deck);
+    if(players > 1)
+    {
+        Player* p2 = new Player(deck);
+        if(players > 2)
+        {
+            Player* p3 = new Player(deck);
+            if(players > 3)
+            {
+                Player* p4 = new Player(deck);
+                if(players > 4)
+                {
+                    Player* p5 = new Player(deck);
+                    if(players > 5)
+                    {
+                        Player* p6 = new Player(deck);
+                        if(players > 6)
+                        {
+                            Player* p7 = new Player(deck);
+                            if(players == 8)
+                            {
+                                Player* p8 = new Player(deck);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+ int x = 0
+
+for (x = 0; x<players; x++){
+   bool pass = false;
+    while( pass == false){
+
+        if(x==0){
+            pass= p1.pass;}
+            else if (x==1){
+                pass = p2.pass;}
+            else if (x==2){
+                pass = p3.pass;}
+            else if (x==3){
+                pass = p4.pass;}
+            else if (x==4){
+                pass = p5.pass;}
+            else if (x==5){
+                pass = p6.pass;}
+            else if (x==6){
+                pass = p7.pass;}
+            else{
+                pass = p8.pass}
+    }
+}
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+}
---
+++ BlackjackProject-master/Blackjack_Project/player.h
@@ -0,0 +1,187 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+#include <vector>
+//#include <main.cpp>
+using namespace std;
+
+class Player
+{
+public:
+    vector<int> newDeck;
+
+    Player(vector<int> cards)
+    {
+        newDeck = cards;
+        for(int x=0;x<2;x++)
+        {
+            draw(newDeck);
+        }
+        bust = false;
+        isBlackJack();
+    }
+
+    void aceChoose()
+    {
+        int ace = 0;
+
+        do
+        {
+        cout<<"Is this ace a 1 or 11? ";
+        cin>>ace;
+        cout<<endl;
+        } while(ace != 1 && ace != 11);
+
+        score+=ace;
+    }
+
+    void draw(vector<int> deck)
+    {
+        bool drawn = false;
+        int card;
+
+        while(drawn == false)
+        {
+            card = rand() % 13+1;
+
+            if(deck[card-1] > 0)
+            {
+                drawn = true;
+                deck[card-1]--;
+                newDeck = deck;
+            }
+        }
+
+        if(card == 11)
+        {
+            playerCards[playerCards.size()] = "Joker";
+            score += 10;
+        }
+        else if(card == 12)
+        {
+            playerCards[playerCards.size()] = "Queen";
+            score += 10;
+        }
+        else if(card == 13)
+        {
+            playerCards[playerCards.size()] = "King";
+            score += 10;
+        }
+        else if(card == 1)
+        {
+            playerCards[playerCards.size()] = "Ace";
+        }
+        else
+        {
+            playerCards[playerCards.size()] = ""+card;
+            score += card;
+        }
+
+        isBlackJack();
+
+    }
+
+    bool pass()
+    {
+        int ans;
+        vector<string> cards = getCards();
+
+        for(unsigned x=0;x<playerCards.size();x++)
+        {
+            if(playerCards[x] == "Ace")
+            {
+                aceChoose();
+            }
+        }
+        isBust();
+
+        if(bust == false)
+        {
+            if(hasBJ)
+            {
+                cout<<"You have Black Jack!";
+            }
+            cout<<"Your Cards are "<<cards[0]<<" "<<endl;
+            cout<<"Your current score is: "<<score;
+            for(unsigned x=1;x<cards.size();x++)
+            {
+                cout<<cards[x]<<" ";
+            }
+            cout<<endl;
+
+            do
+            {
+                cout<<"Would you like to pass? (1 = Yes, 2 = no)";
+                cin>>ans;
+            } while(ans != 1 && ans != 2);
+
+            if(ans == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            cout<<"You are over 21, You bust";
+            return true;
+        }
+
+    }
+
+    vector<string> getCards()
+    {
+        return playerCards;
+    }
+
+    bool isBust()
+    {
+        if(score > 21)
+        {
+            bust = true;
+        }
+        else
+        {
+            bust = false;
+        }
+
+        return bust;
+    }
+
+    void isBlackJack()
+    {
+        bool ace;
+        bool tenCard;
+
+        for(unsigned x=0; x<playerCards.size();x++)
+        {
+            if(playerCards[x] == "Ace")
+            {
+                ace = true;
+            }
+            else if(playerCards[x] == "Jack" || playerCards[x] == "Queen" || playerCards[x] == "King" || playerCards[x] == "10")
+            {
+                tenCard = true;
+            }
+        }
+
+        if(ace == true && tenCard == true)
+        {
+            hasBJ = true;
+        }
+
+        hasBJ = false;
+    }
+
+private:
+    vector<string> playerCards;
+    bool hasBJ;
+    bool bust;
+    int score;
+};
+
+#endif // PLAYER_H
